6afb88a161034a0e1d4b52fdbda8aeb839afa65e
EmmittMendez <77180330+EmmittMendez@users.noreply.github.com>
2025-05-27 01:26:43 -0600

    agregamos linea 31 a la 40

--- index.js
+++ index.js
@@ -28,15 +28,15 @@ app.use("/api", authRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/orders", orderRoutes);
 
-const path = require("path");
+// const path = require("path");
 
-// Servir archivos estáticos del frontend (por ejemplo, Vite)
-app.use(express.static(path.join(__dirname, "dist")));
+// // Servir archivos estáticos del frontend (por ejemplo, Vite)
+// app.use(express.static(path.join(__dirname, "dist")));
 
-// Catch-all: cualquier ruta que no sea API devuelve el index.html del frontend
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
-});
+// // Catch-all: cualquier ruta que no sea API devuelve el index.html del frontend
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "dist", "index.html"));
+// });
 
 // Conexión a la base de datos
 const PORT = process.env.PORT || 3000;
